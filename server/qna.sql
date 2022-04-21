98c21ef0cb1e06dba977c47872c60842f7fa8eff
Charlie <charliebailey24@gmail.com>
2022-04-20 23:09:19 -0700

    updated schema for transformation

--- server/qna.sql
+++ server/qna.sql
@@ -1,51 +1,44 @@
 
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS photos CASCADE;
 
 CREATE TABLE products (
-  id SERIAL NOT NULL,
-  product_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE users (
-  id SERIAL NOT NULL,
-  name TEXT NOT NULL,
-  email TEXT,
+  id INT NOT NULL,
   PRIMARY KEY(id)
 );
 
 CREATE TABLE questions (
   id SERIAL NOT NULL,
-  body TEXT NOT NULL,
-  date BIGINT NOT NULL,
-  helpfulness INTEGER NOT NULL,
-  reported SMALLINT NOT NULL,
   product_id INT NOT NULL,
-  user_id INT NOT NULL,
+  body TEXT NOT NULL,
+  date_written BIGINT NOT NULL,
+  asker_name TEXT NOT NULL,
+  asker_email TEXT NOT NULL,
+  reported SMALLINT NOT NULL,
+  helpful INTEGER NOT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY(product_id) REFERENCES products(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(product_id) REFERENCES products(id)
 );
 
 CREATE TABLE answers (
   id SERIAL NOT NULL,
-  body TEXT NOT NULL,
-  date BIGINT NOT NULL,
-  helpfulness INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+  date_written BIGINT NOT NULL,
+  answerer_name TEXT NOT NULL,
+  answerer_email TEXT NOT NULL,
+  reported SMALLINT NOT NULL,
+  helpful INTEGER NOT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY(question_id) REFERENCES questions(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE photos (
   id SERIAL NOT NULL,
-  url TEXT NOT NULL,
   answer_id INTEGER NOT NULL,
+  url TEXT NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY(answer_id) REFERENCES answers(id)
 );
