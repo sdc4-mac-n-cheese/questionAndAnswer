3a21f6165313e8f894b6afab344c91bf006bb425
Charlie <charliebailey24@gmail.com>
2022-04-21 15:03:24 -0700

    installed the pg driver and connected to the database

---
+++ database/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+client.connect();
+
+client.query('SELECT * FROM questions WHERE id = 1', (err, res) => {
+  console.log(err ? err.stack : res.rows[0]);
+  client.end();
+})
