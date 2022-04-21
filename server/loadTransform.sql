633604473d8b1f2b3a798b8e45c41f8bcb261d99
Charlie <charliebailey24@gmail.com>
2022-04-21 15:22:42 -0700

    documenting my load and transform commands

---
+++ server/loadTransform.sql
@@ -0,0 +1,33 @@
+/* ran these commands from the command line in my local database*/
+
+COPY questions_staging(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+FROM '/Users/charliebailey24/Downloads/questions.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY answers_staging(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
+FROM '/Users/charliebailey24/Downloads/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY photos_staging(id, answer_id, url)
+FROM '/Users/charliebailey24/Downloads/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+INSERT INTO products (id)
+  SELECT DISTINCT product_id
+  FROM questions_staging
+  ORDER BY product_id;
+
+INSERT INTO questions (product_id, body, date_written, asker_name, asker_email, reported, helpful)
+  SELECT product_id, body, date_written, asker_name, asker_email, reported, helpful
+  FROM questions_staging;
+
+INSERT INTO answers (question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
+  SELECT question_id, body, date_written, answerer_name, answerer_email, reported, helpful
+  FROM answers_staging;
+
+INSERT INTO photos (answer_id, url)
+  SELECT answer_id, url
+  FROM photos_staging;
