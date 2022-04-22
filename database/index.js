18aab8025f6ceaab626178ffbd6cf7b337e18203
Charlie <charliebailey24@gmail.com>
2022-04-21 23:54:06 -0700

    attempting to get the data back in the same format as the old api

--- database/index.js
+++ database/index.js
@@ -9,9 +9,39 @@ const client = new Client({
   port: process.env.PORT,
 });
 
-// just test to see if I can get data back from my local server
+// const offset = 40344;
+
+// get the questions for a certain product id
 client.connect();
-client.query('SELECT * FROM questions WHERE id = 1', (err, res) => {
-  console.log(err ? err.stack : res.rows[0]);
+
+let productId = 1;
+
+//(SELECT COUNT(*) FROM product as p WHERE p.store_id=s.id)
+
+// I want to have my "answers" column itself be a query for the answers data at 'the current' question_id
+
+let query = `SELECT questions.id AS question_id, questions.body AS question_body, to_timestamp(questions.date_written) AS question_date, questions.asker_name, questions.helpful AS question_helpfulness, questions.reported, answers.id AS id, answers.body AS body, to_timestamp(answers.date_written) AS date, answers.answerer_name, answers.helpful AS helpfulness
+  FROM questions
+  JOIN answers ON questions.id = answers.question_id
+  WHERE questions.product_id = ${productId}
+  AND questions.reported = 0`
+
+client.query(query, (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    let result = {
+      product_id: productId,
+      results: res.rows
+    }
+
+    // let question = results.question_id;
+
+    console.log('RESULT:::', result);
+  }
   client.end();
 });
+
+module.exports = {
+  client: client
+};
