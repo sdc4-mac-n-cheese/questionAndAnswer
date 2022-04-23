762a541c00b3ba23c3916d5d35fe4b1b6e006ad2
Charlie <charliebailey24@gmail.com>
2022-04-22 17:36:02 -0700

    Initial query getting my main questions route working with js (unoptimized)

--- database/index.js
+++ database/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 
-const client = new Client({
+// const client = new Client({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT,
+// });
+
+const pool = new Pool({
   user: process.env.USER,
   host: process.env.HOST,
   database: process.env.DATABASE,
@@ -9,39 +17,68 @@ const client = new Client({
   port: process.env.PORT,
 });
 
-// const offset = 40344;
+const getAllQuestions = (productId, cb) => {
+  pool.connect();
 
-// get the questions for a certain product id
-client.connect();
-
-let productId = 1;
-
-//(SELECT COUNT(*) FROM product as p WHERE p.store_id=s.id)
-
-// I want to have my "answers" column itself be a query for the answers data at 'the current' question_id
-
-let query = `SELECT questions.id AS question_id, questions.body AS question_body, to_timestamp(questions.date_written) AS question_date, questions.asker_name, questions.helpful AS question_helpfulness, questions.reported, answers.id AS id, answers.body AS body, to_timestamp(answers.date_written) AS date, answers.answerer_name, answers.helpful AS helpfulness
-  FROM questions
-  JOIN answers ON questions.id = answers.question_id
-  WHERE questions.product_id = ${productId}
-  AND questions.reported = 0`
-
-client.query(query, (err, res) => {
-  if (err) {
-    console.log(err.stack);
-  } else {
-    let result = {
-      product_id: productId,
-      results: res.rows
-    }
-
-    // let question = results.question_id;
-
-    console.log('RESULT:::', result);
+  const data = {
+    product_id: productId
   }
-  client.end();
-});
 
-module.exports = {
-  client: client
-};
+  let questionsQuery = `SELECT id AS question_id, questions.body AS question_body, to_timestamp(date_written) AS question_date, asker_name, helpful AS question_helpfulness, reported
+    FROM questions
+    WHERE product_id = ${productId}
+    AND reported = 0`
+
+  pool.query(questionsQuery, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      const questions = res.rows;
+
+      for (let i = 0; i < questions.length; i++) {
+        let questionId = questions[i].question_id;
+
+        let answersQuery = `SELECT id AS id, body AS body, to_timestamp(date_written) AS date, answerer_name, helpful AS helpfulness
+          FROM answers
+          WHERE question_id = ${questionId}
+          AND reported = 0`
+
+        pool.query(answersQuery, (err, res) => {
+          if (err) {
+            cb(err);
+          } else {
+            const answers = res.rows;
+            questions[i].answers = {};
+
+            for (let j = 0; j < answers.length; j++) {
+              let answerId = answers[j].id;
+
+              if (answers[j].id) {
+                questions[i].answers[answers[j].id] = answers[j];
+
+                let photosQuery = `SELECT url FROM photos WHERE answer_id = ${answerId}`;
+
+                pool.query(photosQuery, (err, res) => {
+                  if (err) {
+                    cb(err);
+                  } else {
+                    const photos = res.rows;
+                    questions[i].answers[answers[j].id].photos = photos.map(photo => photo.url);
+
+                    if (i === questions.length - 1 && j === answers.length - 1) {
+                      data.results = questions;
+                      cb(null, data);
+                      // client.end();
+                    }
+                  }
+                });
+              }
+            }
+          }
+        });
+      }
+    }
+  });
+}
+
+module.exports.getAllQuestions = getAllQuestions;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const axios = require('axios');
+const { getAllQuestions } = require('../database/index.js');
 
 const app = express();
 
 const PORT = 3071;
 
+app.use(express.json());
+
+app.get('/qa/questions', (req, res) => {
+  const productId = req.query.product_id;
+
+  getAllQuestions(productId, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else (
+      res.send(results)
+    );
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+// getAllQuestions(1, (err, results) => {
+//   console.log('err:::', err);
+//   console.log('results:::', JSON.stringify(results, null, 4));
+// });
