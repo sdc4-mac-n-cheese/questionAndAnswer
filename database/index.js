d15ab0e214638da72a3b589ae7b9bc9fb2079b35
Charlie <charliebailey24@gmail.com>
2022-04-23 12:03:15 -0700

    First two major routes operational

--- database/index.js
+++ database/index.js
@@ -17,17 +17,20 @@ const pool = new Pool({
   port: process.env.PORT,
 });
 
-const getAllQuestions = (productId, cb) => {
+const getAllQuestions = (productId, page = 1, count = 5, cb) => {
+  const offset = (page - 1) * count;
+
   pool.connect();
 
   const data = {
     product_id: productId
   }
 
-  let questionsQuery = `SELECT id AS question_id, questions.body AS question_body, to_timestamp(date_written) AS question_date, asker_name, helpful AS question_helpfulness, reported
+  let questionsQuery = `SELECT id AS question_id, questions.body AS question_body, to_timestamp(date_written / 1000) AS question_date, asker_name, helpful AS question_helpfulness, reported
     FROM questions
     WHERE product_id = ${productId}
-    AND reported = 0`
+    AND reported = 0
+    LIMIT ${count} OFFSET ${offset}`
 
   pool.query(questionsQuery, (err, res) => {
     if (err) {
@@ -36,9 +39,10 @@ const getAllQuestions = (productId, cb) => {
       const questions = res.rows;
 
       for (let i = 0; i < questions.length; i++) {
-        let questionId = questions[i].question_id;
+        const question = questions[i];
+        const questionId = questions[i].question_id;
 
-        let answersQuery = `SELECT id AS id, body AS body, to_timestamp(date_written) AS date, answerer_name, helpful AS helpfulness
+        let answersQuery = `SELECT id AS id, body AS body, to_timestamp(date_written / 1000) AS date, answerer_name, helpful AS helpfulness
           FROM answers
           WHERE question_id = ${questionId}
           AND reported = 0`
@@ -48,13 +52,14 @@ const getAllQuestions = (productId, cb) => {
             cb(err);
           } else {
             const answers = res.rows;
-            questions[i].answers = {};
+            question.answers = {};
 
             for (let j = 0; j < answers.length; j++) {
-              let answerId = answers[j].id;
+              const answer = answers[j];
+              const answerId = answers[j].id;
 
-              if (answers[j].id) {
-                questions[i].answers[answers[j].id] = answers[j];
+              if (answer.id) {
+                question.answers[answerId] = answer;
 
                 let photosQuery = `SELECT url FROM photos WHERE answer_id = ${answerId}`;
 
@@ -63,7 +68,11 @@ const getAllQuestions = (productId, cb) => {
                     cb(err);
                   } else {
                     const photos = res.rows;
-                    questions[i].answers[answers[j].id].photos = photos.map(photo => photo.url);
+                    question.answers[answerId].photos = photos.map(photo => photo.url);
+
+                    if (question.answers[answerId].photos === undefined) {
+                      question.answers[answerId].photos = [];
+                    }
 
                     if (i === questions.length - 1 && j === answers.length - 1) {
                       data.results = questions;
@@ -81,4 +90,61 @@ const getAllQuestions = (productId, cb) => {
   });
 }
 
-module.exports.getAllQuestions = getAllQuestions;
+const getAllAnswers = (questionId, page = 1, count = 5, cb) => {
+  const offset = (page - 1) * count;
+
+  pool.connect();
+
+  const data = {
+    question: questionId,
+    page: page,
+    count: count,
+  }
+
+  let answersQuery = `SELECT id AS answer_id, body AS body, to_timestamp(date_written / 1000) AS date, answerer_name, helpful AS helpfulness
+    FROM answers
+    WHERE question_id = ${questionId}
+    AND reported = 0
+    LIMIT ${count} OFFSET ${offset}`;
+
+  pool.query(answersQuery, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      const answers = res.rows;
+
+      for (let j = 0; j < answers.length; j++) {
+        const answer = answers[j];
+        const answerId = answers[j]['answer_id'];
+
+        let photosQuery = `SELECT url FROM photos WHERE answer_id = ${answerId}`;
+
+        pool.query(photosQuery, (err, res) => {
+          if (err) {
+            cb(err);
+          } else {
+            const photos = res.rows;
+
+            answer.photos = photos.map(photo => photo.url);
+
+            if (answer.photos === undefined) {
+              answer.photos = [];
+            }
+
+            if (j === answers.length - 1) {
+              data.results = answers;
+              cb(null, data);
+              // client.end();
+            }
+          }
+        });
+      }
+    }
+  });
+
+}
+
+module.exports = {
+  getAllQuestions: getAllQuestions,
+  getAllAnswers: getAllAnswers,
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const { getAllQuestions } = require('../database/index.js');
+const { getAllQuestions, getAllAnswers } = require('../database/index.js');
 
 const app = express();
 
@@ -10,8 +10,23 @@ app.use(express.json());
 
 app.get('/qa/questions', (req, res) => {
   const productId = req.query.product_id;
+  const { page, count } = req.query;
 
-  getAllQuestions(productId, (err, results) => {
+  getAllQuestions(productId, page, count, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else (
+      res.send(results)
+    );
+  });
+});
+
+// this is my anwers route for a given questionId
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  const questionId = req.params.question_id;
+  const { page, count } = req.query;
+
+  getAllAnswers(questionId, page, count, (err, results) => {
     if (err) {
       res.status(500).send(err);
     } else (
@@ -23,8 +38,3 @@ app.get('/qa/questions', (req, res) => {
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
-// getAllQuestions(1, (err, results) => {
-//   console.log('err:::', err);
-//   console.log('results:::', JSON.stringify(results, null, 4));
-// });
