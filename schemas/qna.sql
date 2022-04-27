fec4db3271470c60399085afd3cfe7ec686c1364
Charlie <charliebailey24@gmail.com>
2022-04-27 16:23:11 -0700

    prepped for deployment

--- schemas/qna.sql
+++ schemas/qna.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS questions CASCADE;
 DROP TABLE IF EXISTS answers CASCADE;
 DROP TABLE IF EXISTS photos CASCADE;
 
-CREATE TABLE products (
-  id INT NOT NULL,
-  PRIMARY KEY(id)
-);
+-- CREATE TABLE products (
+--   id INT NOT NULL,
+--   PRIMARY KEY(id)
+-- );
 
 CREATE TABLE questions (
   id SERIAL NOT NULL,
@@ -18,8 +18,8 @@ CREATE TABLE questions (
   asker_email TEXT NOT NULL,
   reported SMALLINT NOT NULL,
   helpful INTEGER NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(product_id) REFERENCES products(id)
+  PRIMARY KEY(id)
+  -- FOREIGN KEY(product_id) REFERENCES products(id)
 );
 
 CREATE TABLE answers (
